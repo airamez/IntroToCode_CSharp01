ae026e2d186f1d214e1faad25b922fe87a8786bb
Jose Santos <josesantos@microsoft.com>
2022-12-07 21:11:37 -0800

    Improvements

--- _00_HelloWorld.cs
+++ _00_HelloWorld.cs
@@ -5,7 +5,5 @@ class HelloWorld
     public static void Main(string[] args)
     {
       Console.WriteLine("Hello World!");
-      int i = 10;
-      Console.WriteLine(i);
     }
 }
---
+++ _01_Comments.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class Comments {
+    public static void Main (string[] args) {
+        Console.WriteLine("Hello World with Comments");
+    }
+}
