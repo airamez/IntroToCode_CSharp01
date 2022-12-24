1b734a720d500e15bbbe379cd0048e426782d8ef
Jose Santos <josesantos@microsoft.com>
2022-12-24 09:48:38 -0800

    Improvements

--- _004_InputOutput.cs
+++ _004_InputOutput.cs
@@ -9,13 +9,26 @@
   - https://learn.microsoft.com/en-us/dotnet/api/system.convert?view=net-7.0
 - String interpolation
   - https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/tokens/interpolated
-  - 
  */
 using System;
 class IO
 {
     public static void Main(string[] args)
     {
-
+      // Console.Write("Name: ");
+      // string userName = Console.ReadLine();
+      // Console.Write("Olá ");
+      // Console.WriteLine(userName);
+      // int i = 45675657;
+      // long l = Int64.MaxValue;
+      // i = (int)l;
+      Console.Write("Name: ");
+      string name = Console.ReadLine();
+      Console.Write("Age: ");
+      string ageAsString = Console.ReadLine();
+      int age = Convert.ToInt16(ageAsString);
+      //string message = "Hello " + name + " your age is " + age + ".";
+      string message =  $"Hello {name} your age is {age}.";
+      Console.WriteLine(message);
     }
 }
--- _005_BasicOperations.cs
+++ _005_BasicOperations.cs
@@ -15,5 +15,30 @@ class BasicOperations
 {
     public static void Main(string[] args)
     {
+      // int counter = 0;
+      // counter += 1;
+      // counter += 3; // counter = counter + 3
+      // counter -= 2; // counter = counter - 2
+      // counter++; // counter += 1
+      // int test1 = counter++;
+      // int test2 = ++counter;
+      int a = 1;
+      int b = 2;
+      int c = 7;
+      int sum = a + b - c;
+
+      double d = c / b;
+      d = c / (double) b;
+      c = 15;
+      b = 4;
+      int rest = c % b;
+
+      // Console.WriteLine(c > b);
+      // Console.WriteLine(c == b);
+      // Console.WriteLine(c >= b);
+      // Console.WriteLine(c <= b);
+      // Console.WriteLine(c != b);
+
+      double average = (a + b + c) / (double)3;
     }
 }
