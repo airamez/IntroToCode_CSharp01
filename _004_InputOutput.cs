73f3af6ed6dcd4a0e4a955e2e9b9c86f03c81314
Jose Santos <josesantos@microsoft.com>
2022-12-24 07:42:00 -0800

    Improvements

---
+++ _004_InputOutput.cs
@@ -0,0 +1,21 @@
+/*
+- Console Class
+  - https://learn.microsoft.com/en-us/dotnet/api/system.console?view=net-7.0
+- Input/Read
+- Output/Write
+- Casting
+  - https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/types/casting-and-type-conversions
+- Convertion
+  - https://learn.microsoft.com/en-us/dotnet/api/system.convert?view=net-7.0
+- String interpolation
+  - https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/tokens/interpolated
+  - 
+ */
+using System;
+class IO
+{
+    public static void Main(string[] args)
+    {
+
+    }
+}
