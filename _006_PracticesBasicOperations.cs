6d58c472fbb5d4b7029f6c264c3dabb1c061ad63
Jose Santos <josesantos@microsoft.com>
2022-12-30 14:45:47 -0800

    Improvements

--- _006_PracticesBasicOperations.cs
+++ _006_PracticesBasicOperations.cs
@@ -1,20 +1,23 @@
 /*
 - Practices:
   - Create programs to:
-    - Read a value in miles and convert to kilometers: 1 Mile ~= 1.6KM.
-    - Read an integer number and print the predecessor and successor.
-    - Read two integer numbers A and B and print the result of arithmetic operations you know.
+    - Read a value in miles and convert to kilometers: 1 Mile = 1.60934KM.
+    - Read an integer number and print the three predecessors and successors.
+    - Read two integer numbers A and B and print the result of all arithmetic operations you know for A and B.
     - Read three integer numbers and calculate the average.
-    - Read the length and breadth of a rectangle and find its area.
+    - Read the base and length of a rectangle and calculate its area.
     - Read an integer and print True if the number is Even and False if it is Odd.
     - Read a temperature in Celsius and convert to Fahrenheit: F = C x 1.8 + 32.
-    - Read the buying price (cost), the desired profit percentage and calculate the sales price of a product.
-    - Read the name, salary and salary increase percentage and print the name and the new salary.
-    - Read the name. salary, years of experience and numbers of kids and calculate the name and the salary using the
-      following formula: 0.5% per year of experience and 2% per kid.
-    - Read an integer number representing minutes and print how many hours and minutes it represents.
-    - Read a number of days and print how many years, weeks and days.
-  - 
+    - Read the buying price (cost), the desired profit percentage and
+      calculate the sales price of a product.
+    - Read the name, salary and salary increase percentage and 
+      print the name and the new salary.
+    - Read the name, salary, years of experience and numbers of kids and
+      calculate the name and the new salary using the following formula: 
+        - 0.5% per year of experience
+        - 2% per kid
+    - Read the duration of an experiment in seconds and 
+      calculate the duration in Hours, Minutes and Seconds.
  */
 using System;
 class PracticesBasicOperations
