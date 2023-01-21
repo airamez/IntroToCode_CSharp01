67965c00258cc15d9bbff777160225b29965b907
airamez <airamez@gmail.com>
2023-01-21 09:49:28 -0800

    Solving Questions 10 and 11

--- _006_BasicOperationsQuestion10.cs
+++ _006_BasicOperationsQuestion10.cs
@@ -1,7 +1,22 @@
+/*
+10. Read the name, salary and salary increase percentage and 
+    print the name and the new salary.
+ */
 using System;
 class _006_BasicOperationsQuestions10
 {
     public static void Main(string[] args)
     {
+        Console.Write("Name: ");
+        string name = Console.ReadLine();
+        Console.Write("Salary: ");
+        double salary = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Salary Increase %: ");
+        double salaryIncreasePercentage = Convert.ToDouble(Console.ReadLine());
+
+        double salaryIncrease = salary * (salaryIncreasePercentage / 100);
+        double newSalary = salary + salaryIncrease;
+
+        Console.WriteLine($"Name: {name}; New Salary: {newSalary}; Salary Increase: {salaryIncrease}");
     }
 }
