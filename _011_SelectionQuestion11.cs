87cfcb8efbd5d255e6534c3331a666fcf989ca3c
airamez <airamez@gmail.com>
2023-03-18 09:17:02 -0700

    New question resolution

--- _011_SelectionQuestion11.cs
+++ _011_SelectionQuestion11.cs
@@ -1,7 +1,32 @@
+/*
+Read the current hour (military time: 0 to 23) and print the appropriate salutation:
+    Good morning
+    Good Afternoon
+    Good evening
+    Good night
+ */
 using System;
 class SelectionQuestion11
 {
     public static void Main(string[] args)
     {
+        Console.Write("Hour = ");
+        int hour = Convert.ToInt32(Console.ReadLine());
+
+        if (hour < 0 || hour > 23) {
+            Console.WriteLine("Invalid hour!");
+        } else {
+            string salutation;
+            if (hour < 12) {
+                salutation = "Morning";
+            } else if (hour < 16) {
+                salutation = "Afternoon";
+            } else if (hour < 20) {
+                salutation = "Evening";
+            } else {
+                salutation = "Night";
+            }
+            Console.WriteLine($"Good {salutation}!!");
+        }
     }
 }
--- _011_SelectionQuestion12.cs
+++ _011_SelectionQuestion12.cs
@@ -1,7 +1,39 @@
+/*
+Read a single digit number and print the description (name).
+ */
 using System;
 class SelectionQuestion12
 {
     public static void Main(string[] args)
     {
+        Console.Write("Digit = ");
+        int digit = Convert.ToInt32(Console.ReadLine());
+        string digitName;
+        if (digit < 0 || digit > 9) {
+            Console.WriteLine("Invalid digit!");
+        } else {
+            if (digit == 0) {
+                digitName = "Zero";
+            } else if (digit == 1) {
+                digitName = "One";
+            } else if (digit == 2) {
+                digitName = "Two";
+            } else if (digit == 3) {
+                digitName = "Three";
+            } else if (digit == 4) {
+                digitName = "Four";
+            } else if (digit == 5) {
+                digitName = "Five";
+            } else if (digit == 6) {
+                digitName = "Six";
+            } else if  (digit == 7) {
+                digitName = "Seven";
+            } else if (digit == 8) {
+                digitName = "Eight";
+            } else {
+                digitName = "Nine";
+            }
+            Console.WriteLine(digitName);
+        }
     }
 }
