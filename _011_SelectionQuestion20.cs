82770a80e43565dc02d2ceedae3e2c3b01a57430
airamez <airamez@gmail.com>
2023-05-06 20:24:35 -0700

    Class #19

--- _011_SelectionQuestion20.cs
+++ _011_SelectionQuestion20.cs
@@ -1,7 +1,36 @@
+/*
+20. Read the name and salary of a employee and calculate the new salary
+    based on the rules below:
+    Salary               Raising %
+     0 to 50,000           20%
+     50,001 to 100,000     15%
+     100,001 to 150,000    10%
+     150,000 +              5%
+     Print the name, raising % and the new Salary
+ */
 using System;
 class SelectionQuestion20
 {
     public static void Main(string[] args)
     {
+        Console.Write("Name: ");
+        string name = Console.ReadLine();
+        Console.Write("Salary: ");
+        double salary = Convert.ToDouble(Console.ReadLine());
+
+        int raisingPercentual = 0;
+        if (salary <= 50000) {
+            raisingPercentual = 20;
+        } else if (salary <= 100000) {
+            raisingPercentual = 15;
+        } else if (salary <= 150000) {
+            raisingPercentual = 10;
+        } else {
+            raisingPercentual = 5;
+        }
+        double salaryIncrease = salary * (raisingPercentual / 100.00);
+        double newSalary = salary + salaryIncrease;
+
+        Console.WriteLine($"Name: {name}; Salary: {newSalary}; Increase: {salaryIncrease}");
     }
 }
