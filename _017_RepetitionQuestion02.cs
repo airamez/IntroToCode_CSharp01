0e7a791a63b82d6be9beee983a9b36cde27839d8
airamez <airamez@gmail.com>
2023-05-27 11:18:42 -0700

    Class 23

---
+++ _017_RepetitionQuestion02.cs
@@ -0,0 +1,51 @@
+/*
+ * Read two integer numbers representing an 
+ interval and print all numbers inside the interval.
+ */
+using System;
+class RepetitionQuestion02
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Minimum: ");
+        int min = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Maximum: ");
+        int max = Convert.ToInt32(Console.ReadLine());
+
+        // Making sure the interval is in ascending order
+        // if  (min > max) {
+        //     int aux = min;
+        //     min = max;
+        //     max = aux;
+        // }
+        // for (int i = min; i <= max; i++) {
+        //     Console.Write($"{i},");
+        // }
+
+        // if (min <= max) {
+        //     for (int i = min; i <= max; i++) {
+        //         Console.Write($"{i},");
+        //     }   
+        // } else {
+        //     for (int i = max; i <= min; i++) {
+        //         Console.Write($"{i},");
+        //     }
+        // }
+        Console.WriteLine();
+
+        if (min <= max) {
+            for (int i = min; i <= max; i++) {
+                Console.Write($"{i},");
+            }
+        } else {
+            if  (min > max) {
+                int aux = min;
+                min = max;
+                max = aux;
+            }
+            for (int i = max; i >= min; i--) {
+                Console.Write($"{i},");
+            }
+        }
+    }
+}
---
+++ _017_RepetitionQuestion03.cs
@@ -0,0 +1,30 @@
+/*
+Read two integer numbers representing an interval and 
+print all even numbers inside the interval.
+*/
+using System;
+class RepetitionQuestion03
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Minimum: ");
+        int min = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Maximum: ");
+        int max = Convert.ToInt32(Console.ReadLine());
+
+        // for (int i = min; i <= max; i++) {
+        //     if (i % 2 == 0) {
+        //         Console.Write($"{i},");
+        //     }
+        // }
+
+        Console.WriteLine();
+
+        if (min % 2 != 0) {
+            min++;
+        }
+        for (int i = min; i <= max; i += 2) {
+            Console.Write($"{i},");
+        }
+    }
+}
