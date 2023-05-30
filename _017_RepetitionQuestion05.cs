e20e8a30f9d3e70625b5056c2d9a15a36449d03d
airamez <airamez@gmail.com>
2023-05-29 20:23:07 -0700

    Class #25

--- _017_RepetitionQuestion05.cs
+++ _017_RepetitionQuestion05.cs
@@ -1,10 +1,33 @@
 /*
- * Read integer numbers (flag = empty) and print the highest value.
+ * Read integer numbers (flag = empty) and 
+   print the highest value.
  */
 using System;
 class RepetitionQuestion05
 {
     public static void Main(string[] args)
     {
+        string numberStr;
+        int number;
+        int highest = int.MinValue;
+        bool numberEntered = false;
+        do {
+            Console.Write("Number: ");
+            numberStr = Console.ReadLine();
+            // string.Empty == ""
+            if (numberStr != string.Empty) {
+                numberEntered = true;
+                number = Convert.ToInt32(numberStr);
+                highest = Math.Max(highest, number);
+                // if (number > highest) {
+                //     highest = number;
+                // }
+            }
+        } while (numberStr != string.Empty);
+        if (numberEntered) {
+            Console.WriteLine($"Highest number is: {highest}");
+        } else {
+            Console.WriteLine("No numbers entered");
+        }
     }
 }
