31944346ade1c4a156bdc6b5ff5732f24aa9b418
airamez <airamez@gmail.com>
2023-06-10 18:33:45 -0700

    Clas #27

--- _017_RepetitionQuestion07.cs
+++ _017_RepetitionQuestion07.cs
@@ -6,6 +6,29 @@ class RepetitionQuestion07
 {
     public static void Main(string[] args)
     {
+        string numberStr;
+        int number;
+        int? lowest = null;
+        int lowsestFrequency = 0;
 
+        do {
+            Console.Write("Number: ");
+            numberStr = Console.ReadLine();
+            if (numberStr != string.Empty) {
+                number = Convert.ToInt32(numberStr);
+                if ( (!lowest.HasValue) ||
+                     number < lowest) {
+                    lowest = number;
+                    lowsestFrequency = 1;
+                } else if (number == lowest) {
+                    lowsestFrequency++;
+                }
+            }
+        } while(numberStr != string.Empty);
+        if (lowest.HasValue) {
+            Console.Write($"Lowest = {lowest}; Frequency = {lowsestFrequency}");
+        } else {
+            Console.Write("No values provided");
+        }
     }
 }
