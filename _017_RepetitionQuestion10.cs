b739687dd9528fc0d533f9403ab406159e27b349
airamez <airamez@gmail.com>
2023-06-18 17:24:07 -0700

    Class #31

--- _017_RepetitionQuestion10.cs
+++ _017_RepetitionQuestion10.cs
@@ -13,6 +13,35 @@ class RepetitionQuestion10
 {
     public static void Main(string[] args)
     {
+        Random rnd = new Random();
+        int corrects = 0;
+        int incorrects = 0;
+        int number1 = 0;
+        int number2 = 0;
+        int result = 0;
+        string guessStr;
+        int guess = 0;
 
+        while (true) {
+            number1 = rnd.Next(10);
+            number2 = rnd.Next(10);
+            result = number1 * number2;
+            Console.Write($"{number1} x {number2} = ");
+            guessStr = Console.ReadLine();
+            if (guessStr == string.Empty) {
+                break;
+            }
+            guess = Convert.ToInt32(guessStr);
+            if (guess == result) {
+                corrects++;
+            } else {
+                incorrects++;
+            }
+            if (incorrects == 5) {
+                break;
+            }
+        }
+        Console.WriteLine($"Correct answers = {corrects}");
+        Console.WriteLine($"Incorrect answers = {incorrects}");
     }
 }
