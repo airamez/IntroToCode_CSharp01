c3a7de9d5b50ef84ab7cf347be3b2162d813f74b
airamez <airamez@gmail.com>
2023-06-25 12:28:04 -0700

    Class #34

--- _017_RepetitionQuestion13.cs
+++ _017_RepetitionQuestion13.cs
@@ -9,5 +9,19 @@ class RepetitionQuestion13
 {
     public static void Main(string[] args)
     {
+        Console.Write("Number: ");
+        long number = Convert.ToInt64(Console.ReadLine());
+        long numberAux = number;
+        long numberInverted = 0;
+
+        while (numberAux != 0) {
+            // Extract last digit
+            int lastDigit = (int) numberAux % 10;
+            // Add last digit to the end of the inverted number
+            numberInverted = numberInverted * 10 + lastDigit;
+            // Update the numberAux to all digits but the last
+            numberAux /= 10; // NumberAux = NumberAux / 10;
+        }
+        Console.WriteLine($"The inverse of {number} is {numberInverted}");
     }
 }
