c5997bc93d45f68c4228346f1a542b2ab982d86b
airamez <airamez@gmail.com>
2023-07-01 17:45:28 -0700

    Class #35

--- _017_RepetitionQuestion14.cs
+++ _017_RepetitionQuestion14.cs
@@ -6,5 +6,20 @@ class RepetitionQuestion14
 {
     public static void Main(string[] args)
     {
+        Console.Write("Number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        bool isPrime = true;
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime && number != 1) {
+            Console.WriteLine($"{number } is Prime");
+        } else {
+            Console.WriteLine($"{number } is NOT Prime");
+        }
     }
 }
