3cc9cdd8a8b00d350d4194705a26ff7b550983ca
airamez <airamez@gmail.com>
2023-07-02 22:07:37 -0700

    Class #36

--- _017_RepetitionQuestion15.cs
+++ _017_RepetitionQuestion15.cs
@@ -1,10 +1,43 @@
 /*
-15. Read two integer numbers and calculate the "integer" division without use the division operator.
+15. Read two integer numbers and calculate the "integer" division 
+    without use the division operator.
 */
 using System;
 class RepetitionQuestion15
 {
     public static void Main(string[] args)
     {
+        Console.Write("Dividend: ");
+        int dividend = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Divisor: ");
+        int divisor = Convert.ToInt32(Console.ReadLine());
+
+        if (divisor == 0) {
+            Console.WriteLine("Sorry but I can't divide by zero");
+            return;
+        }
+        
+        int originalDividend = dividend;
+        int originalDivisor = divisor;
+        dividend = Math.Abs(dividend);
+        divisor = Math.Abs(divisor);
+
+        int remainder = dividend;
+        int quocient = 0;
+        while (remainder >= divisor) {
+            quocient++;
+            remainder -= divisor;
+        }
+        // Setting the quocient to negative when:
+        //   dividend positive and divisor negative
+        //                     Or
+        //   dividend negativa and divisor positive
+        if ((originalDividend >= 0 && originalDivisor < 0) ||
+            (originalDividend < 0 && originalDivisor >= 0)) {
+                quocient = -quocient;
+            }
+
+        Console.WriteLine($"{originalDividend} / {originalDivisor} = {quocient}");
+        Console.WriteLine($"{originalDividend} / {originalDivisor}: Quocient = {quocient}; Remainder = {remainder}");
     }
 }
