c7dd37a0b4a01bcd8923eccc547d118c2f85129d
airamez <airamez@gmail.com>
2023-07-09 21:56:59 -0700

    Classes #36 and #37

--- _017_RepetitionQuestion16.cs
+++ _017_RepetitionQuestion16.cs
@@ -1,5 +1,6 @@
 /*
-16. Read an integer number H and print H lines composed of * characters start with one character and adding one * for each new line.
+16. Read an integer number H and print H lines composed of * characters start 
+    with one character and adding one * for each new line.
     e.g.
     Height = 7
     *
@@ -15,5 +16,22 @@ class RepetitionQuestion16
 {
     public static void Main(string[] args)
     {
+        Console.Write("Height: ");
+        int height = Convert.ToInt32(Console.ReadLine());
+
+        // Option 1
+        for (int row = 1; row <= height; row++) {
+            for (int i = 1; i <= row; i++) {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        // Option 2
+        string asterics = "*";
+        for (int row = 1; row <= height; row++) {
+            Console.WriteLine(asterics);
+            asterics += "*";
+        }
     }
 }
