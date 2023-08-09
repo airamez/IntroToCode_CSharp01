d4539566bd637f0ffacbe394d6d3ba3c75845ed6
airamez <airamez@gmail.com>
2023-08-08 21:11:54 -0700

    Improving organization by using folders

---
+++ _04_Modulatization/_01_Intro.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace company.domain.project.module;
+public class ModularizationIntro
+{
+    public static void Main (string[] args) {
+        Console.WriteLine("Hello world of sub-routines");
+    }
+}
