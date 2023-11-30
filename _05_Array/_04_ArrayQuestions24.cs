abef2454a71164892be22e784774482ba6a951ad
Jose Santos <airamez@gmail.com>
2023-11-29 21:54:32 -0800

    Class #80: Array question 24

---
+++ _05_Array/_04_ArrayQuestions24.cs
@@ -0,0 +1,38 @@
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using OurCompany;
+
+/*
+ * 24. Implement a routine to return the longest sequence of duplicated elements
+       of an array of integers containing only 0s and 1s
+ */
+public class ArrayQuestion24
+{
+    public static void Main(string[] args)
+    {
+        int[] array = {1,0,1,1,0,0,0,1,0,1,1,1,1,1,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0};
+        int longest = GetLongestSequenceLength(array);
+        Console.WriteLine(longest);
+    }
+
+    private static int GetLongestSequenceLength(int[] array)
+    {
+        int longest = 1;
+        int longestLocal = 1;
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            if (array[i] == array[i+1]) {
+                longestLocal++;
+            } else {
+                longestLocal = 1;
+            }
+            if (longestLocal > longest) {
+                longest = longestLocal;
+            }
+        }
+        return longest;
+    }
+}
