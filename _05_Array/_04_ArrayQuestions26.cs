8a5e90958cc9162b98a3ad6bb7ca60cce5feb92b
Jose Santos <airamez@gmail.com>
2023-12-01 21:03:52 -0800

    Class #82: Array question 26

---
+++ _05_Array/_04_ArrayQuestions26.cs
@@ -0,0 +1,39 @@
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Runtime.Serialization.Formatters;
+using OurCompany;
+
+/*
+ * 26. Implement a routine to randomize an array of integers
+ */
+public class ArrayQuestion26
+{
+    public static void Main(string[] args)
+    {
+        int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        RandomizeArray(array);
+        MyArray.Print(array);
+
+        RandomizeArray(array);
+        MyArray.Print(array);
+
+        for (int j = 0; j < 10000; j++)
+        {
+            RandomizeArray(array);
+            MyArray.Print(array, $"#{j}");
+        }
+    }
+
+    private static void RandomizeArray(int[] array)
+    {
+        Random random = new Random();
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            int randomIndex = random.Next(i, array.Length);
+            MyArray.swap(array, i, randomIndex);
+        }
+    }
+}
