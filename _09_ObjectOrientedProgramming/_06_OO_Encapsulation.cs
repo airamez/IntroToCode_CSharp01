d0d8c664bb101f33f5e484531dbe4ae457690104
Jose Santos <airamez@gmail.com>
2024-01-01 20:07:43 -0800

    Add class content

---
+++ _09_ObjectOrientedProgramming/_06_OO_Encapsulation.cs
@@ -0,0 +1,16 @@
+using System;
+
+/*
+# Encapsulation is the wrapping up of data and information inside a class,
+  and offer methods to access them.
+# It is achieved by declaring variables as private and using properties
+  to access them.
+# There are four access modifiers that can be used to control the visibility:
+   public: Anywhere
+   private: Only inside the class
+   protected: Only inside the class and sub-classes
+   internal: Only inside the same Assembly (EXE, DLL or Project)
+# Class Content:
+  # 1. Review Name Space
+  # 2. Create a new class Car
+ */
---
+++ _09_ObjectOrientedProgramming/_07_OO_Property.cs
@@ -0,0 +1,26 @@
+using System;
+
+/*
+# Properties are used to encapsulate fields and provide a way to access them.
+# They are similar to methods, but they are accessed like fields.
+# There are two types of properties: auto-implemented properties and custom properties.
+# Auto-implemented properties have a get and a set accessor, but no implementation. 
+  The compiler will automatically generate a backing field for the property.
+# Custom properties have a get and a set accessor, and a custom implementation. 
+  This allows you to perform additional logic when the property is accessed or modified.
+
+# Class Content:
+  # 1. Attribute with Set and Get
+  # 2. Custom Property
+  # 3. Auto-implemented Property
+  # 4. Auto-implemented Property with "expression-bodied syntax"
+ */
+
+namespace OurCompany.LearnCoding.OOP.Property {
+
+    public class PropertyApp {
+    }
+
+    public class Car {
+    }
+}
