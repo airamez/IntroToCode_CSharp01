57f2005bbe67a2f16e024aad4db27997525be3f6
Jose Santos <airamez@gmail.com>
2024-01-28 18:44:38 -0800

    Add content for the relationship class

---
+++ _09_ObjectOrientedProgramming/_19_OO_Relationships.cs
@@ -0,0 +1,37 @@
+/*
+# Relationship is a way to describe how one class is connected to another class.
+# Consequently, how object are connected as well.
+# There are several types of relationships between classes:
+  # Direct reference: In this type of relationship, one class has a direct 
+    reference to another class. 
+  # Association: This is a more general relationship between classes.
+    # It simply means that two classes are related in some way. 
+    # The association can be:
+      # One to One: 1:1
+      # One to Many: 1:N
+      # Many to Many1: N:N
+  # Aggregation: This is a type of association where one class is a part of 
+    another class. For example, if you have a Person class and an Address class,
+    you might say that a Person has an aggregation relationship with an Address.
+  # Composition: This is a stronger form of aggregation where one class is composed
+    of one or more other classes. For example, if you have a Car class and classes 
+    Wheel, Engine and Transmission, you might say that a Car has a composition
+    relationship with these classes.
+
+# Class Content:
+  # Show examples using Visio digrams
+  # Show some implementations
+  # Quick overview about the class List
+*/
+
+
+using System;
+
+namespace OurCompany.LearnCoding.OOP.Relationship;
+
+public class RelationshipApp {
+
+    public static void Main (string[] args) {
+
+    }
+}
