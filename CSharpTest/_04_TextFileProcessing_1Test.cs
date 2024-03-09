ab0ca3e1b02e461f94b6a7223490c18904d74b8d
José Santos <airamez@gmail.com>
2024-03-08 18:55:04 -0800

    Fixing

--- CSharpTest/_04_TextFileProcessing_1Test.cs
+++ CSharpTest/_04_TextFileProcessing_1Test.cs
@@ -6,30 +6,26 @@ using UnitTesting1;
 [TestFixture]
 public class TextFileProcessing_1Test
 {
-    [Test]
-    public void TestMainMethod()
+  [Test]
+  public void TestMainMethod()
+  {
+    // Arrange
+    string expectedOutput = "1: 10" + System.Environment.NewLine +
+                            "2: 20" + System.Environment.NewLine +
+                            "3: 30" + System.Environment.NewLine +
+                            "Invalid value" + System.Environment.NewLine +
+                            "4: 40" + System.Environment.NewLine +
+                            "Sum: 100" + System.Environment.NewLine +
+                            "Average: 25";
+    // Act
+    string actualOutput;
+    using (StringWriter writer = new StringWriter())
     {
-        // Arrange
-        string expectedOutput = "1: 10" + System.Environment.NewLine +
-                                "2: 20" + System.Environment.NewLine +
-                                "3: 30" + System.Environment.NewLine +
-                                "Invalid value" + System.Environment.NewLine +
-                                "4: 40" + System.Environment.NewLine +
-                                "Sum: 100" + System.Environment.NewLine +
-                                "Average: 25";
-        // Act
-        string actualOutput;
-        using (StringWriter writer = new StringWriter())
-        {
-            Console.SetOut(writer);
-            TextFileProcessing_1.Main(null);
-            actualOutput = writer.ToString().Trim();
-            Console.WriteLine(expectedOutput);
-            Console.WriteLine(actualOutput);
-        }
-        // Assert
-        //TODO: Explain the Actual and Expected values: You made a confusion last class :(
-        //TODO: Fix the warnings below
-        Assert.AreEqual(expectedOutput, actualOutput);
+      Console.SetOut(writer);
+      TextFileProcessing_1.Main(null);
+      actualOutput = writer.ToString().Trim();
     }
+    // Assert
+    Assert.That(actualOutput, Is.EqualTo(expectedOutput));
+  }
 }
