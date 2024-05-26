0826bb6974969bed66cab4f86b9dd2c8a774abe2
Jose Santos <airamez@gmail.com>
2024-05-26 12:21:23 -0700

    Adding the Binary tree files

---
+++ CSharp/_14_DataStructures/_09_BinaryTree.cs
@@ -0,0 +1,17 @@
+/*
+# Tree Traversals
+  # In-Order: left subtree -> node -> right subtree.
+  # Pre-Order: node -> left subtree -> right subtree.
+  # Post-Order: left subtree -> right subtree -> node
+  # Level-Order: starting from root, transverse each level from left to right
+*/
+
+using System;
+namespace DataStructures.BinaryTree;
+
+public class BinaryTreeApp
+{
+    public static void Main(String[] args)
+    {
+    }
+}
