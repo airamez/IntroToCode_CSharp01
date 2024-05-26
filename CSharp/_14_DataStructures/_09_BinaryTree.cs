793b3542efc7489109181c1fa75a410c95eb327b
Jose Santos <airamez@gmail.com>
2024-05-26 13:29:48 -0700

    Class #175: Data Structures - Binary Tree (part 1)

--- CSharp/_14_DataStructures/_09_BinaryTree.cs
+++ CSharp/_14_DataStructures/_09_BinaryTree.cs
@@ -1,17 +1,51 @@
-/*
-# Tree Traversals
-  # In-Order: left subtree -> node -> right subtree.
-  # Pre-Order: node -> left subtree -> right subtree.
-  # Post-Order: left subtree -> right subtree -> node
-  # Level-Order: starting from root, transverse each level from left to right
-*/
-
 using System;
 namespace DataStructures.BinaryTree;
 
 public class BinaryTreeApp
 {
-    public static void Main(String[] args)
+  public static void Main(string[] args)
+  {
+    Node root = new Node("A");
+    root.Left = new Node("B");
+    root.Right = new Node("C");
+    root.Left.Left = new Node("D");
+    root.Left.Right = new Node("E");
+    root.Right.Left = new Node("F");
+    root.Right.Right = new Node("G");
+    root.Left.Left.Left = new Node("H");
+    root.Left.Left.Right = new Node("I");
+    root.Left.Right.Left = new Node("J");
+    root.Left.Right.Right = new Node("K");
+    root.Right.Left.Left = new Node("L");
+    root.Right.Left.Right = new Node("M");
+    root.Right.Right.Left = new Node("N");
+    root.Right.Right.Right = new Node("O");
+
+    TransversalInOrder(root);
+  }
+
+  public static void TransversalInOrder(Node node)
+  {
+    if (node == null)
     {
+      return;
     }
+    TransversalInOrder(node.Left);
+    Console.Write($"{node.Data} ");
+    TransversalInOrder(node.Right);
+  }
+}
+
+public class Node
+{
+  public string Data { set; get; }
+  public Node Left { set; get; }
+  public Node Right { set; get; }
+
+  public Node(string data)
+  {
+    this.Data = data;
+    Left = null;
+    Right = null;
+  }
 }
