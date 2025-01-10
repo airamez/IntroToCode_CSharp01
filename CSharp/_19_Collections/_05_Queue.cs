a7811ea5e58803f930c5d183b493eddaa75d756f
Jose Santos <airamez@gmail.com>
2025-01-09 20:08:16 -0800

    Aula 227: Generic Collections - Queue and Stack

--- CSharp/_19_Collections/_05_Queue.cs
+++ CSharp/_19_Collections/_05_Queue.cs
@@ -2,7 +2,6 @@
 https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.queue-1?view=net-8.0
 */
 using System;
-using System.Collections;
 using System.Collections.Generic;
 
 namespace Collections;
@@ -16,7 +15,14 @@ public class QueueApp
     queue.Enqueue("Second");
     queue.Enqueue("Third");
     queue.Enqueue("Fourth");
+
+    Console.WriteLine(queue.Count);
+
+    var item = queue.Dequeue();
+    Console.WriteLine(item);
+
     Console.WriteLine(queue.Peek());
+
     while (queue.Count > 0)
     {
       Console.WriteLine(queue.Dequeue());
--- CSharp/_19_Collections/_06_Stack.cs
+++ CSharp/_19_Collections/_06_Stack.cs
@@ -2,7 +2,6 @@
 https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.stack-1?view=net-8.0
 */
 using System;
-using System.Collections;
 using System.Collections.Generic;
 
 namespace Collections;
@@ -12,11 +11,18 @@ public class StackApp
   public static void Main(string[] args)
   {
     var stack = new Stack<string>();
-    stack.Push("First");
-    stack.Push("Second");
-    stack.Push("Third");
     stack.Push("Fourth");
+    stack.Push("Third");
+    stack.Push("Second");
+    stack.Push("First");
+
+    Console.WriteLine(stack.Count);
+
+    var item = stack.Pop();
+    Console.WriteLine(item);
+
     Console.WriteLine(stack.Peek());
+
     while (stack.Count > 0)
     {
       Console.WriteLine(stack.Pop());
