7f55673a31db6c2e454c86d24c61c6e2025f20ea
Jose Santos <airamez@gmail.com>
2025-01-11 19:24:38 -0800

    HashSet content

---
+++ CSharp/_19_Collections/_07_HashSet.cs
@@ -0,0 +1,76 @@
+// https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.hashset-1?view=net-9.0
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataStructures;
+
+public class HashSetApp
+{
+  public static void Main(string[] args)
+  {
+    var odd = new HashSet<int>();
+    odd.Add(1);
+    odd.Add(3);
+    odd.Add(5);
+    odd.Add(7);
+    odd.Add(9);
+    odd.Add(1);
+    odd.Add(1);
+    odd.Add(9);
+    odd.Add(9);
+    Console.WriteLine(odd.Count);
+    PrintSet(odd);
+
+    Console.WriteLine(odd.Contains(1)); // Show documentation comparing to List
+    Console.WriteLine(odd.Contains(2));
+
+    var even = new HashSet<int>();
+    even.Add(0);
+    even.Add(2);
+    even.Add(4);
+    even.Add(6);
+    even.Add(8);
+    PrintSet(even);
+
+    var all = new HashSet<int>(even);
+    all.UnionWith(odd);
+    Console.WriteLine(all.Count);
+    PrintSet(all);
+
+    all.IntersectWith(even);
+    PrintSet(all);
+
+    // Difference
+    var setA = new HashSet<int>();
+    setA.Add(1);
+    setA.Add(2);
+    setA.Add(3);
+    setA.Add(4);
+    var setB = new HashSet<int>();
+    setB.Add(1);
+    setB.Add(4);
+    var diff = new HashSet<int>(setA);
+    diff.ExceptWith(setB);
+    PrintSet(diff);
+
+    PrintSet(even);
+
+    Console.WriteLine(even.Count(e => e > 5));
+    Console.WriteLine(even.All(e => e % 2 == 0));
+    Console.WriteLine(odd.All(e => e % 2 == 0));
+    Console.WriteLine(even.Max());
+    Console.WriteLine(even.Min());
+    Console.WriteLine(even.Average());
+    Console.WriteLine(even.Sum());
+  }
+
+  private static void PrintSet(HashSet<int> all)
+  {
+    foreach (int n in all)
+    {
+      Console.Write($"{n} ");
+    }
+    Console.WriteLine();
+  }
+}
