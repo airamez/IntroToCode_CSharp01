70fee80f3c757f2e4c5ceba60decd4b1c119bb94
Jose Santos <airamez@gmail.com>
2025-01-16 19:41:39 -0800

    Aula 231: Generic Collections - Dictionary Demo

---
+++ CSharp/_19_Collections/_08_DictionaryDemo.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Collections;
+
+namespace DataStructures;
+
+public class DictionaryDemoApp
+{
+  public static void Main(string[] args)
+  {
+    Dictionary<int, Product> products = new Dictionary<int, Product>();
+    for (int i = 0; i < 1_000_000; i++)
+    {
+      var newProduct = Product.GenerateRandom();
+      products.Add(newProduct.Id, newProduct);
+    }
+
+    while (true)
+    {
+      Console.Write("Product ID: ");
+      string idAsString = Console.ReadLine();
+      if (string.IsNullOrEmpty(idAsString))
+      {
+        break;
+      }
+      int id = int.Parse(idAsString);
+      var product = products.GetValueOrDefault(id);
+      if (product == null)
+      {
+        Console.WriteLine("Id not found:");
+      }
+      else
+      {
+        Console.WriteLine(product);
+      }
+    }
+  }
+}
