57e161e08bd9dad3409e56d061a61b07456f471c
Jose Santos <airamez@gmail.com>
2025-02-02 12:58:24 -0800

    Aula 235: Generic Collections - PriorityQueue (Heap) - Leetcode question 215

--- CSharp/_19_Collections/_10_ProrityQueueDemo.cs
+++ CSharp/_19_Collections/_10_ProrityQueueDemo.cs
@@ -1,4 +1,5 @@
 // https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+// 215. Kth Largest Element in an Array
 
 using System;
 using System.Collections.Generic;
@@ -9,11 +10,31 @@ public class PriorityQueueDemoApp
 {
   public static void Main(string[] args)
   {
-    PriorityQueueDemoApp app = new PriorityQueueDemoApp();
+    /*
+      Example 1:
+      Input: nums = [3,2,1,5,6,4], k = 2
+      Output: 5
+
+      Example 2:
+      Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+      Output: 4
+    */
+    var app = new PriorityQueueDemoApp();
+    Console.WriteLine(app.FindKthLargest([3, 2, 1, 5, 6, 4], 2));
+    Console.WriteLine(app.FindKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4));
   }
 
   public int FindKthLargest(int[] nums, int k)
   {
-    throw new NotImplementedException();
+    var heap = new PriorityQueue<int, int>();
+    foreach (var num in nums)
+    {
+      heap.Enqueue(num, num);
+      if (heap.Count > k)
+      {
+        heap.Dequeue();
+      }
+    }
+    return heap.Dequeue();
   }
 }
